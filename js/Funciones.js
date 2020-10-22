de7498eadf6112403f0d3aa911eadf82fe887de9
JavierOspino <javieralfonso17416@gmail.com>
2020-10-22 04:55:13 -0500

    Validacion Contrasena

--- js/Funciones.js
+++ js/Funciones.js
@@ -1,7 +1,7 @@
 $("#btnValidar").click(function (event) {
   event.preventDefault();
 
-  let re = /([A-Z]{1})([\d]{3})([a-z]{3})([\W]{3})/;
+  let re = /^(([A-Z]{1})([\d]{3})([a-z]{3})([\W]{3}))$/;
   var match = re.test($("#pass").val());
   if (!match) {
     Swal.fire("Contrasena invalida");
